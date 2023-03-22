a9be2572d2feca5291cfb18e2186fb4271ca0ff3
mmd <22621120+mm-ghost@users.noreply.github.com>
2023-03-22 20:49:13 +0330

    Update redis connection data

--- utils.py
+++ utils.py
@@ -1,14 +1,12 @@
+import os
+
 from web3 import Web3
 import redis
 
 w3 = Web3(Web3.HTTPProvider('https://arbitrum.blockpi.network/v1/rpc/public'))
 
-# db = redis.Redis(host='localhost', port=6379, db=0)
-
-db = redis.Redis(
-    host='db-redis-lon1-64155-do-user-13783795-0.b.db.ondigitalocean.com',
-    port=25061,
-    username="default",
-    password="l9IiehUd4w6QpsSSH3F",
-    db=0
-)
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    db = redis.Redis().from_url(database_url)
+else:
+    db = redis.Redis()
